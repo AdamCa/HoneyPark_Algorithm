0cfc25d4d7b9a1a74e0e4c7f7f01455b81643e30
AdamCa <acantor6@gatech.edu>
2015-07-08 11:57:16 -0400

    Environment Development
    Added Parking Lot and Bee List Arrays

--- src/Environment.java
+++ src/Environment.java
@@ -17,34 +17,51 @@ import java.util.*;
  */
 public class Environment {
 
+    public static double bees = 5;
+    public static double lots = 5;
+
     public static void main(String[] args) {
         //create an ArrayList object to hold the Vehicles and the Parking Lots
         ArrayList beeList = new ArrayList();
+        ArrayList lotList = new ArrayList();
 
-        //Add elements to Arraylist
-        beeList.add("1");
-        beeList.add("2");
-        beeList.add("3");
-
+        for(int index=0; index < bees; index++) {
+            //Add elements to beeList
+            Bottles o = new Bottles();
+            o.setbottleID(bottleIntMap.get("bottleID"+i));
+            o.setname_abbr(bottleNamesMap.get("name"+i));
+            o.setorigin(bottleNamesMap.get("origin"+i));
+            o.setbottlePicture(bottleNamesMap.get("bottlePicture"+i));
+            o.setprice_reported(bottleNamesMap.get("price"+i));
+            o.setdistillery(bottleNamesMap.get("distillery"+i));
+            o.setagg_score(bottleIntMap.get("aggscore"+i));
+            beeList.add(index, o);
+        }
     /*
       To add an element at the specified index of ArrayList use
       void add(int index, Object obj) method.
       This method inserts the specified element at the specified index in the
       ArrayList.
-    */
-        beeList.add(1,"INSERTED ELEMENT");
 
-    /*
       Please note that add method DOES NOT overwrites the element previously
       at the specified index in the list. It shifts the elements to right side
       and increasing the list size by 1.
     */
+        for(int index=0; index < lots; index++) {
+            //Add elements to lotList
+            lotList.add(index, new ArrayList());
+        }
 
-        System.out.println("BeeList contains...");
+        System.out.println("beeList contains...");
         //display elements of BeeList
-        for(int index=0; index < beeList.size(); index++)
+        for(int index=0; index < beeList.size(); index++) {
             System.out.println(beeList.get(index));
-
+        }
+        System.out.println("lotList contains...");
+        //display elements of BeeList
+        for(int index=0; index < lotList.size(); index++) {
+            System.out.println(lotList.get(index));
+        }
     }
 }
 
